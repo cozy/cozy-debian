0146f91e7e0fcf781e7ee08ca31b406c9911144a
Nicolas Ledez <github.public@ledez.net>
2016-03-01 16:17:23 +0000

    Fix debian-reconfigure-cozy-domain.sh

--- debian-reconfigure-cozy-domain.sh
+++ debian-reconfigure-cozy-domain.sh
@@ -5,6 +5,8 @@ echo $DOMAIN
 echo 'cozy cozy/certificate text ' | debconf-set-selections
 echo "cozy cozy/fqdn string $DOMAIN" | debconf-set-selections
 
+coffee /usr/local/cozy/apps/home/commands.coffee setdomain $DOMAIN
+
 cozy_management generate_certificate ${DOMAIN}
 
 service nginx restart
